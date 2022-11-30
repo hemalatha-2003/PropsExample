9db876d9b343c48151150c7f321a7134b0adf0a1
GOTTIPALLIRAJESH116 <rajeshgottipalli1122002@gmail.com>
2022-11-30 16:55:59 +0530

    PropsExample 30-11-22

---
+++ src/App.js
@@ -0,0 +1,18 @@
+//import ClassComponent from './ClassComponent'
+import PropsExample from './PropsExample'
+function App(){
+  return(
+    <section>
+        <PropsExample name="hema" age="19"/>
+        <PropsExample name="fcbger6yb" age="21"/>
+        {/*<p>vibe alone until you are valued</p>
+        <h1>heading tag</h1>
+        <img src="https://images.unsplash.com/photo-1599161954112-300ca11ca7da?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW91bnRhaW4lMjByaXZlcnxlbnwwfHwwfHw%3D&w=1000&q=80" width="25%" alt="river" />
+        <ClassComponent/>*/}
+          </section>
+          )
+        }
+  
+
+
+export default App; 
