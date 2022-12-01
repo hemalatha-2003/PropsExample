5f099f3ce791cd4cda576165c3d8d3ab001fc3da
GOTTIPALLIRAJESH116 <rajeshgottipalli1122002@gmail.com>
2022-12-01 16:49:14 +0530

    States&Events concepts

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-//import ClassComponent from './ClassComponent'
-import PropsExample from './PropsExample'
+import Demo from './Demo'
+import Events from './Events'
+//import StateExample from './StateExample'
+//import FuncProps from './FuncProps'
 function App(){
   return(
-    <section>
-        <PropsExample name="hema" age="19"/>
-        <PropsExample name="fcbger6yb" age="21"/>
-        {/*<p>vibe alone until you are valued</p>
-        <h1>heading tag</h1>
-        <img src="https://images.unsplash.com/photo-1599161954112-300ca11ca7da?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW91bnRhaW4lMjByaXZlcnxlbnwwfHwwfHw%3D&w=1000&q=80" width="25%" alt="river" />
-        <ClassComponent/>*/}
-          </section>
+    <div>
+    <Events/>
+    <Demo price="1st price"/>
+    {/*<StateExample/>
+        <FuncProps carname="rollsroyce" year="2017"/>
+        <h1>hii</h1>*/}
+      </div>
           )
         }
   
 
 
-export default App; 
+export default App
